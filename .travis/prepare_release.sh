33cba7c2f06fe4ebcc73e0f39a38517e19f14a7e
shyshkov <ext-adrian.shyshkovskyi@here.com>
2020-02-24 19:59:15 +0200

    Updated Travis CI GIT username and email

--- .travis/prepare_release.sh
+++ .travis/prepare_release.sh
@@ -16,6 +16,10 @@ GIT_RELEASE_TAG="Release-${MAIN_VERSION}"
 sed -i "s/<tag>.*<\/tag>/<tag>${GIT_RELEASE_TAG}<\/tag>/g" pom.xml
 find . -name "pom.xml" -not -path "./maven-wagon-installer/src/*" -not -path "./maven-wagon-installer/target/*" \
   | xargs git add
+
+git config user.name "Travis CI"
+git config user.email "OLP_ENG_LIMERICK@here.com"
+
 git commit -m "[skip ci] Preparing for release $MAIN_VERSION"
 git tag -a "${GIT_RELEASE_TAG}" -m "Release $MAIN_VERSION from build $TRAVIS_BUILD_ID"
 
--- .travis/push_to_github.sh
+++ .travis/push_to_github.sh
@@ -4,9 +4,6 @@ set -ev
 echo "Pushing to GitHub"
 GIT_RELEASE_TAG=$(git describe --abbrev=0)
 
-git config user.name "Travis CI"
-git config user.email "OLP_ENG_LIMERICK@here.com"
-
 git remote add origin-travis https://${GITHUB_TOKEN}@github.com/heremaps/here-artifact-maven-wagon.git
 git push origin-travis "${GIT_RELEASE_TAG}"
 git push origin-travis HEAD:travis-test
