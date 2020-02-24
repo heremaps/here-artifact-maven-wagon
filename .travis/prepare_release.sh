84eb508aa8bef4b63e9b1295e19c498064bf6dcc
shyshkov <ext-adrian.shyshkovskyi@here.com>
2020-02-24 18:07:25 +0200

    Aded prepare release step to Travis CI

--- .travis/prepare_release.sh
+++ .travis/prepare_release.sh
@@ -2,3 +2,30 @@
 set -ev
 
 echo "Preparing release"
+mvn -B build-helper:parse-version versions:set \
+  -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion} \
+  versions:commit
+
+MAIN_VERSION=$(mvn -q \
+    -Dexec.executable=echo \
+    -Dexec.args='${project.version}' \
+    --non-recursive \
+    exec:exec)
+GIT_RELEASE_TAG="Release-${MAIN_VERSION}"
+
+sed -i "s/<tag>.*<\/tag>/<tag>${GIT_RELEASE_TAG}<\/tag>/g" pom.xml
+find . -name "pom.xml" -not -path "./maven-wagon-installer/src/*" -not -path "./maven-wagon-installer/target/*" \
+  | xargs git add
+git commit -m "Preparing for release $MAIN_VERSION"
+git tag -a "${GIT_RELEASE_TAG}" -m "Release $MAIN_VERSION from build $TRAVIS_JOB_ID"
+
+# Export some variables
+cat > environment.properties << EOF
+MAIN_VERSION=${MAIN_VERSION}
+GIT_RELEASE_TAG=${GIT_RELEASE_TAG}
+EOF
+
+# TEMP
+git log
+git tag -n9
+cat environment.properties
