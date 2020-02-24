7e4910cb7cf40212d332296f2bb76ec371461d3f
shyshkov <ext-adrian.shyshkovskyi@here.com>
2020-02-24 19:51:56 +0200

    Added Git push Travis step implementation

--- .travis/push_to_github.sh
+++ .travis/push_to_github.sh
@@ -2,11 +2,11 @@
 set -ev
 
 echo "Pushing to GitHub"
+GIT_RELEASE_TAG=$(git describe --abbrev=0)
+
 git config user.name "Travis CI"
 git config user.email "OLP_ENG_LIMERICK@here.com"
 
 git remote add origin-travis https://${GITHUB_TOKEN}@github.com/heremaps/here-artifact-maven-wagon.git
-GIT_RELEASE_TAG=$(git describe --abbrev=0)
-echo "${GIT_RELEASE_TAG}"
-# git push origin-travis "${GIT_RELEASE_TAG}"
-# git push origin-travis HEAD:travis-test
+git push origin-travis "${GIT_RELEASE_TAG}"
+git push origin-travis HEAD:travis-test
