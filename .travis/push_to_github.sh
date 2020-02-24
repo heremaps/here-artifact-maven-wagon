a2542c12156faba4698846abaf462fd3940d2c40
shyshkov <ext-adrian.shyshkovskyi@here.com>
2020-02-24 19:47:42 +0200

    Testing Git push Travis CI step

--- .travis/push_to_github.sh
+++ .travis/push_to_github.sh
@@ -6,6 +6,7 @@ git config user.name "Travis CI"
 git config user.email "OLP_ENG_LIMERICK@here.com"
 
 git remote add origin-travis https://${GITHUB_TOKEN}@github.com/heremaps/here-artifact-maven-wagon.git
+GIT_RELEASE_TAG=$(git describe --abbrev=0)
 echo "${GIT_RELEASE_TAG}"
 # git push origin-travis "${GIT_RELEASE_TAG}"
 # git push origin-travis HEAD:travis-test
