7511191f4530ded2f20d75e96ae0cc3abf490d11
Adrian Shyshkovskyi <ext-adrian.shyshkovskyi@here.com>
2020-03-13 17:44:31 +0200

    Added version increment to Travis CI release flow based on latest tag
    Signed-off-by: Adrian Shyshkovskyi <ext-adrian.shyshkovskyi@here.com>

--- .travis/scripts/prepare_release.sh
+++ .travis/scripts/prepare_release.sh
@@ -2,6 +2,20 @@
 set -ev
 
 # Prepare release
+LATEST_RELEASE_TAG=$(git describe --abbrev=0)
+if [[ $LATEST_RELEASE_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  # Set current released version
+  mvn -B -q versions:set -DnewVersion=$LATEST_RELEASE_TAG
+
+  # Increment patch version
+  mvn -B -q build-helper:parse-version versions:set \
+  -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion} \
+  versions:commit
+else
+  echo "Cannot parse the latest release tag: ${LATEST_RELEASE_TAG}"
+  exit 1
+fi
+
 RELEASE_VERSION=$(mvn -q \
     -Dexec.executable=echo \
     -Dexec.args='${project.version}' \
