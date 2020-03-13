8ced76bf6b477590655897ee6ca7055184a8312c
Adrian Shyshkovskyi <ext-adrian.shyshkovskyi@here.com>
2020-03-13 18:13:01 +0200

    Renamed Travis CI release flow variables and added comments to pom.xml
    Signed-off-by: Adrian Shyshkovskyi <ext-adrian.shyshkovskyi@here.com>

--- .travis/scripts/prepare_release.sh
+++ .travis/scripts/prepare_release.sh
@@ -2,21 +2,21 @@
 set -ev
 
 # Prepare release
-LATEST_RELEASE_TAG=$(git describe --abbrev=0)
-if [[ $LATEST_RELEASE_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+PREVIOUS_RELEASE_TAG=$(git describe --abbrev=0)
+if [[ $PREVIOUS_RELEASE_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
   # Set current released version
-  mvn -B -q versions:set -DnewVersion=$LATEST_RELEASE_TAG
+  mvn -B -q versions:set -DnewVersion=$PREVIOUS_RELEASE_TAG
 
   # Increment patch version
   mvn -B -q build-helper:parse-version versions:set \
   -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion} \
   versions:commit
 else
-  echo "Cannot parse the latest release tag: ${LATEST_RELEASE_TAG}"
+  echo "Cannot parse the latest release tag: ${PREVIOUS_RELEASE_TAG}"
   exit 1
 fi
 
-RELEASE_VERSION=$(mvn -q \
+RELEASE_TAG=$(mvn -q \
     -Dexec.executable=echo \
     -Dexec.args='${project.version}' \
     --non-recursive \
@@ -25,7 +25,7 @@ RELEASE_VERSION=$(mvn -q \
 git config user.name "Travis CI"
 git config user.email "OLP_ENG_LIMERICK@here.com"
 
-git tag -a "${RELEASE_VERSION}" -m "Release $RELEASE_VERSION from build $TRAVIS_BUILD_ID"
+git tag -a "${RELEASE_TAG}" -m "Release $RELEASE_TAG from build $TRAVIS_BUILD_ID"
 
 git remote add origin-travis https://${GITHUB_TOKEN}@${GITHUB_REPO}
-git push origin-travis "${RELEASE_VERSION}"
+git push origin-travis "${RELEASE_TAG}"
