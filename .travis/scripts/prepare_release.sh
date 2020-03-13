2fc82ab4d87ccb0dc9b4d2bf0e1c72502d301487
Adrian Shyshkovskyi <ext-adrian.shyshkovskyi@here.com>
2020-03-13 16:48:35 +0200

    Removed version increment step from Travis CI release flow
    Signed-off-by: Adrian Shyshkovskyi <ext-adrian.shyshkovskyi@here.com>

--- .travis/scripts/prepare_release.sh
+++ .travis/scripts/prepare_release.sh
@@ -2,22 +2,16 @@
 set -ev
 
 # Prepare release
-mvn -B -q build-helper:parse-version versions:set \
-  -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion} \
-  versions:commit
-
 RELEASE_VERSION=$(mvn -q \
     -Dexec.executable=echo \
     -Dexec.args='${project.version}' \
     --non-recursive \
     exec:exec)
-GIT_RELEASE_TAG="Release-${RELEASE_VERSION}"
-
-sed -i "s/<tag>.*<\/tag>/<tag>${GIT_RELEASE_TAG}<\/tag>/g" pom.xml
-find . -name "pom.xml" | xargs git add
 
 git config user.name "Travis CI"
 git config user.email "OLP_ENG_LIMERICK@here.com"
 
-git commit -m "[skip ci] Preparing for release $RELEASE_VERSION"
-git tag -a "${GIT_RELEASE_TAG}" -m "Release $RELEASE_VERSION from build $TRAVIS_BUILD_ID"
+git tag -a "${RELEASE_VERSION}" -m "Release $RELEASE_VERSION from build $TRAVIS_BUILD_ID"
+
+git remote add origin-travis https://${GITHUB_TOKEN}@${GITHUB_REPO}
+git push origin-travis "${RELEASE_VERSION}"
