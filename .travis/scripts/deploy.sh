a742618fac0ec99564b1d37578bcb17d22979b47
Thomas Steenbergen <thomas.steenbergen@here.com>
2020-03-13 09:54:11 +0100

    Release to maven central
    Signed-off-by: Thomas Steenbergen <thomas.steenbergen@here.com>

--- .travis/scripts/deploy.sh
+++ .travis/scripts/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -ev
 
+# Decrypt gpg key
+openssl aes-256-cbc -K $encrypted_4f116abdd4b6_key -iv $encrypted_4f116abdd4b6_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
+# Import gpg key
+gpg --fast-import .travis/codesigning.asc
 # Deploy to Maven Central
-
-# Uncomment the line below when ready
-# mvn --settings .travis/travis-settings.xml clean deploy -Prelease
+mvn --settings .travis/travis-settings.xml clean deploy -Prelease
