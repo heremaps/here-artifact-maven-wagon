f6548922ffdf4ba4e61971d696823b1cd2eb6063
Oleksandr Vyshniak <ext-oleksandr.vyshniak@here.com>
2022-09-22 10:06:13 +0300

    Use Lookup API for Artifact Service URL resolution
    Signed-off-by: Oleksandr Vyshniak <ext-oleksandr.vyshniak@here.com>

---
+++ src/main/java/com/here/platform/artifact/maven/wagon/model/LookupPlatformApisResponse.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2017-2022 HERE Global B.V. and its affiliate(s).
+ * All rights reserved.
+ *
+ * This software and other materials contain proprietary information
+ * controlled by HERE and are protected by applicable copyright legislation.
+ * Any use and utilization of this software and other materials and
+ * disclosure to any third parties is conditional upon having a separate
+ * agreement with HERE for the access, use, utilization or disclosure of this
+ * software. In the absence of such agreement, the use of the software is not
+ * allowed.
+ */
+
+package com.here.platform.artifact.maven.wagon.model;
+
+/**
+ * Lookup Service platform API response model.
+ */
+public class LookupPlatformApisResponse {
+
+  private String api;
+  private String version;
+  private String baseURL;
+  private Object parameters;
+
+  public String getApi() {
+    return api;
+  }
+
+  public void setApi(String api) {
+    this.api = api;
+  }
+
+  public String getVersion() {
+    return version;
+  }
+
+  public void setVersion(String version) {
+    this.version = version;
+  }
+
+  public String getBaseURL() {
+    return baseURL;
+  }
+
+  public void setBaseURL(String baseURL) {
+    this.baseURL = baseURL;
+  }
+
+  public Object getParameters() {
+    return parameters;
+  }
+
+  public void setParameters(Object parameters) {
+    this.parameters = parameters;
+  }
+}
